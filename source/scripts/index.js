de5d674ef6ff17243af83680dd799c0eaab4cdb6
viktoriyafv <152386470+viktoriyafv@users.noreply.github.com>
2024-10-16 14:15:24 +0300

    header (#1)
    * установка зависимостей
    
    * header

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,9 @@
 /* в этот файл добавляет скрипты*/
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.onclick = function () {
+  navMain.classList.toggle('main-nav--opened');
+};
